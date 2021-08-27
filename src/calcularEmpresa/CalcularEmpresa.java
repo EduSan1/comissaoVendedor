dd42e9e56abe5d7baa6ee3cd6ee6e5a7214ef60f
Eduardo Santos Nascimento <88251709+EduSan1@users.noreply.github.com>
2021-08-27 16:16:45 -0300

    atualização

--- src/calcularEmpresa/CalcularEmpresa.java
+++ src/calcularEmpresa/CalcularEmpresa.java
@@ -1,3 +1,48 @@
+<<<<<<< HEAD
+package calcularEmpresa;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class CalcularEmpresa {
+
+	public static void main(String[] args) {
+
+		double comissão, salario, valor;
+		int tempo;
+
+		DecimalFormat duas = new DecimalFormat("0.00");
+
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Insira o nome do(a) vendedor(a): ");
+		String nome = entrada.next();
+		System.out.print("A quantos anos o(a) " + nome + " trabalha na empresa? ");
+		tempo = entrada.nextInt();
+		System.out.print("Qual o valor total de vendas do(a)" + nome + "? ");
+		valor = entrada.nextDouble();
+		entrada.close();
+
+		if (tempo >= 1 && valor >= 10000 && valor < 20000) {
+
+			comissão = 0.05;
+
+		} else if (tempo >= 1 && valor >= 20000) {
+
+			comissão = 0.1;
+
+		} else {
+
+			comissão = 0.03;
+
+		}
+
+		salario = comissão * valor;
+		System.out.println("A comissão de " + nome + " é: " + comissão);
+		System.out.println("e o valor que deverá receber é: " + duas.format(salario));
+	}
+
+}
+=======
 package calcularEmpresa;
 
 import java.util.Scanner;
@@ -39,3 +84,4 @@ public class CalcularEmpresa {
 	}
 
 }
+>>>>>>> ea93c8c8ce04a173fe2b210e6787625f7d0d3917
