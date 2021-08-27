b685759fb9774101e8fa91cfcd2559f6932ecdd2
Eduardo Santos Nascimento <88251709+EduSan1@users.noreply.github.com>
2021-08-27 16:19:31 -0300

    atualizações certas

--- src/calcularEmpresa/CalcularEmpresa.java
+++ src/calcularEmpresa/CalcularEmpresa.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package calcularEmpresa;
 
 import java.text.DecimalFormat;
@@ -42,46 +41,3 @@ public class CalcularEmpresa {
 	}
 
 }
-=======
-package calcularEmpresa;
-
-import java.util.Scanner;
-
-public class CalcularEmpresa {
-
-	public static void main(String[] args) {
-
-		double comissão;
-		double salario;
-
-		Scanner entrada = new Scanner(System.in);
-		System.out.print("Insira o nome do(a) vendedor(a): ");
-		String nome = entrada.next();
-		System.out.print("A quantos anos o(a) " + nome + " trabalha na empresa? ");
-		int tempo = entrada.nextInt();
-		System.out.print("Qual o valor total de vendas do(a)" + nome + "? ");
-		double valor = entrada.nextDouble();
-		entrada.close();
-
-		if (tempo >= 1 && valor >= 10000 && valor < 20000) {
-
-			comissão = 0.05;
-
-		} else if (tempo >= 1 && valor >= 20000) {
-
-			comissão = 0.10;
-
-		} else {
-
-			comissão = 0.03;
-
-		}
-
-		salario = comissão * valor;
-		System.out.println("A comissão de " + nome + " é: " + comissão);
-		System.out.println("e o valor que deverá receber é: " + salario);
-
-	}
-
-}
->>>>>>> ea93c8c8ce04a173fe2b210e6787625f7d0d3917
